832fdbc535a8fda203301d534f948b3acd3f9a83
lizanbing <zanbingli@gmail.com>
2022-07-04 15:16:49 +0800

    部分练习

--- exercises/rectangles/src/lib.rs
+++ exercises/rectangles/src/lib.rs
@@ -1,3 +1,48 @@
+extern crate itertools;
+
+use itertools::Itertools;
+
 pub fn count(lines: &[&str]) -> u32 {
-    unimplemented!("\nDetermine the count of rectangles in the ASCII diagram represented by the following lines:\n{:#?}\n.", lines);
+    if lines.is_empty() { return 0; }
+    if lines[0].is_empty() { return 0; }
+    let row = lines.len();
+    let col = lines[0].len();
+
+    let mut vec = vec![];
+    for i in 0..row {
+        for j in 0..col {
+            if let Some('+') = lines[i].chars().nth(j) {
+                vec.push((i, j));
+            }
+        }
+    }
+    vec.into_iter()
+        .combinations(4)
+        .filter(|x| is_valid_rect(x, lines))
+        .count() as u32
+}
+
+fn is_valid_rect(vec: &Vec<(usize, usize)>, lines: &[&str]) -> bool {
+    if vec.len() != 4 { return false; }
+    let p1: &(usize, usize) = &vec[0];
+    let p2: &(usize, usize) = &vec[1];
+    let p3: &(usize, usize) = &vec[2];
+    let p4: &(usize, usize) = &vec[3];
+
+    if p1.0 == p2.0 && p1.1 == p3.1 && p2.1 == p4.1 && p3.0 == p4.0 {
+        for i in p1.0 + 1..p3.0 {
+            match lines[i].chars().nth(p1.1) {
+                Some('-')=>return false,
+                Some(' ')=>return false,
+                _=>(),
+            }
+            match lines[i].chars().nth(p2.1) {
+                Some('-')=>return false,
+                Some(' ')=>return false,
+                _=>(),
+            }
+        }
+        return true;
+    }
+    false
 }
--- exercises/spiral-matrix/src/lib.rs
+++ exercises/spiral-matrix/src/lib.rs
@@ -1,6 +1,96 @@
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    unimplemented!(
-        "Function that returns the spiral matrix of square size {}",
-        size
-    );
+    let mut rt = vec![vec![0; size as usize]; size as usize];
+    if size == 0 {
+        return rt;
+    }
+    let mut pos = (0, 0, Dir::East);
+    let mut num = 2;
+    rt[0][0] = 1;
+    loop {
+        if let Some(v) = get_new_pos(&pos, &rt) {
+            rt[v.0 as usize][v.1 as usize] = num;
+            pos = v.clone();
+            num += 1;
+        } else {
+            break;
+        }
+    }
+    rt
+}
+
+#[derive(Clone)]
+pub enum Dir {
+    East,
+    South,
+    West,
+    North,
+}
+
+fn get_new_pos(pos: &(i32, i32, Dir), matrix: &Vec<Vec<u32>>) -> Option<(i32, i32,
+                                                                         Dir)> {
+    let len = matrix.len() as i32;
+    match pos.2 {
+        Dir::East => {
+            if is_valid_pos(&(pos.0, pos.1 + 1), len, matrix) {
+                return Some((pos.0, pos.1 + 1, Dir::East));
+            }
+            if is_valid_pos(&(pos.0 + 1, pos.1), len, matrix) {
+                return Some((pos.0 + 1, pos.1, Dir::South));
+            }
+            if is_valid_pos(&(pos.0, pos.1 - 1), len, matrix) {
+                return Some((pos.0, pos.1 - 1, Dir::West));
+            }
+            if is_valid_pos(&(pos.0 - 1, pos.1), len, matrix) {
+                return Some((pos.0 - 1, pos.1, Dir::North));
+            }
+        }
+        Dir::South => {
+            if is_valid_pos(&(pos.0 + 1, pos.1), len, matrix) {
+                return Some((pos.0 + 1, pos.1, Dir::South));
+            }
+            if is_valid_pos(&(pos.0, pos.1 - 1), len, matrix) {
+                return Some((pos.0, pos.1 - 1, Dir::West));
+            }
+            if is_valid_pos(&(pos.0 - 1, pos.1), len, matrix) {
+                return Some((pos.0 - 1, pos.1, Dir::North));
+            }
+            if is_valid_pos(&(pos.0, pos.1 + 1), len, matrix) {
+                return Some((pos.0, pos.1 + 1, Dir::East));
+            }
+        }
+        Dir::West => {
+            if is_valid_pos(&(pos.0, pos.1 - 1), len, matrix) {
+                return Some((pos.0, pos.1 - 1, Dir::West));
+            }
+            if is_valid_pos(&(pos.0 - 1, pos.1), len, matrix) {
+                return Some((pos.0 - 1, pos.1, Dir::North));
+            }
+            if is_valid_pos(&(pos.0, pos.1 + 1), len, matrix) {
+                return Some((pos.0, pos.1 + 1, Dir::East));
+            }
+            if is_valid_pos(&(pos.0 + 1, pos.1), len, matrix) {
+                return Some((pos.0 + 1, pos.1, Dir::South));
+            }
+        }
+        Dir::North => {
+            if is_valid_pos(&(pos.0 - 1, pos.1), len, matrix) {
+                return Some((pos.0 - 1, pos.1, Dir::North));
+            }
+            if is_valid_pos(&(pos.0, pos.1 + 1), len, matrix) {
+                return Some((pos.0, pos.1 + 1, Dir::East));
+            }
+            if is_valid_pos(&(pos.0 + 1, pos.1), len, matrix) {
+                return Some((pos.0 + 1, pos.1, Dir::South));
+            }
+            if is_valid_pos(&(pos.0, pos.1 - 1), len, matrix) {
+                return Some((pos.0, pos.1 - 1, Dir::West));
+            }
+        }
+    }
+    None
+}
+
+fn is_valid_pos(pos: &(i32, i32), len: i32, matrix: &Vec<Vec<u32>>) -> bool {
+    pos.0 >= 0 && pos.0 < len && pos.1 >= 0 && pos.1 < len && matrix[pos.0 as usize][pos.1 as usize]
+        == 0
 }
