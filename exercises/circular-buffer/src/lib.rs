42ad1ea01726087b953b317be5c694934e49e825
lizanbing <zanbingli@gmail.com>
2022-07-12 13:52:44 +0800

    循环缓冲区和ocr

--- exercises/circular-buffer/src/lib.rs
+++ exercises/circular-buffer/src/lib.rs
@@ -1,11 +1,8 @@
-use std::marker::PhantomData;
-
 pub struct CircularBuffer<T> {
-    // This field is here to make the template compile and not to
-    // complain about unused type parameter 'T'. Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
-    field: PhantomData<T>,
+    field: Vec<T>,
+    read_ptr: usize,
+    write_ptr: usize,
+    caps: usize,
 }
 
 #[derive(Debug, PartialEq)]
@@ -14,30 +11,50 @@ pub enum Error {
     FullBuffer,
 }
 
-impl<T> CircularBuffer<T> {
+impl<T> CircularBuffer<T>
+    where T: Clone + Default {
     pub fn new(capacity: usize) -> Self {
-        unimplemented!(
-            "Construct a new CircularBuffer with the capacity to hold {}.",
-            match capacity {
-                1 => format!("1 element"),
-                _ => format!("{} elements", capacity),
-            }
-        );
-    }
-
-    pub fn write(&mut self, _element: T) -> Result<(), Error> {
-        unimplemented!("Write the passed element to the CircularBuffer or return FullBuffer error if CircularBuffer is full.");
+        CircularBuffer {
+            field: vec![T::default(); capacity + 1],
+            read_ptr: 0,
+            write_ptr: 0,
+            caps: capacity + 1,
+        }
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        unimplemented!("Read the oldest element from the CircularBuffer or return EmptyBuffer error if CircularBuffer is empty.");
+        if self.field.is_empty() || self.write_ptr == self.read_ptr {
+            return Err(Error::EmptyBuffer);
+        }
+        let value = self.field[self.read_ptr].clone();
+        self.read_ptr = (self.read_ptr + 1) % self.caps;
+        Ok(value)
+    }
+
+    pub fn write(&mut self, _element: T) -> Result<(), Error> {
+        if self.is_full() {
+            return Err(Error::FullBuffer);
+        }
+        self.field[self.write_ptr] = _element;
+        self.write_ptr = (self.write_ptr + 1) % self.caps;
+        Ok(())
     }
 
     pub fn clear(&mut self) {
-        unimplemented!("Clear the CircularBuffer.");
+        self.read_ptr = 0;
+        self.write_ptr = 0;
     }
 
     pub fn overwrite(&mut self, _element: T) {
-        unimplemented!("Write the passed element to the CircularBuffer, overwriting the existing elements if CircularBuffer is full.");
+        if self.is_full()  {
+            self.read_ptr = (self.read_ptr + 1) % self.caps;
+        }
+        self.field[self.write_ptr] = _element;
+        self.write_ptr = (self.write_ptr + 1) % self.caps;
+    }
+
+    pub fn is_full(&self) -> bool {
+        // (self.write_ptr + 1) % self.caps == self.read_ptr
+        (self.read_ptr as i32 - self.write_ptr as i32 + self.caps as i32) % self.caps as i32 == 1
     }
 }
--- exercises/ocr-numbers/src/lib.rs
+++ exercises/ocr-numbers/src/lib.rs
@@ -1,6 +1,7 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidRowCount(usize),
@@ -8,5 +9,60 @@ pub enum Error {
 }
 
 pub fn convert(input: &str) -> Result<String, Error> {
-    unimplemented!("Convert the input '{}' to a string", input);
+    let nums: Vec<&str> = input.split("\n").collect();
+    if nums.len() % 4 != 0 { return Err(Error::InvalidRowCount(nums.len())); }
+    let mut cols = 0;
+    for it in nums.iter() {
+        if cols == 0 {
+            cols = it.len();
+        }
+        if it.len() % 3 != 0 || it.len() != cols {
+            return Err(Error::InvalidColumnCount(it.len()));
+        }
+    }
+    let mut rt: Vec<String> = vec![];
+    for it in nums.chunks(4) {
+        if it.len() == 4 {
+            if let Some(v) = it.first() {
+                let mut index = 0;
+                let mut ts = String::new();
+                while index < v.len() {
+                    ts.push(
+                        convert_character(&it[0][index..index + 3].chars()
+                            .chain(it[1][index..index + 3].chars())
+                            .chain(it[2][index..index + 3].chars())
+                            .chain(it[3][index..index + 3].chars()).collect()));
+                    index += 3;
+                }
+                rt.push(ts);
+            }
+        }
+    }
+    Ok(rt.join(","))
+}
+
+fn convert_character(input: &Vec<char>) -> char {
+    if &input[..] == [' ', '_', ' ', '|', ' ', '|', '|', '_', '|', ' ', ' ', ' '] {
+        '0'
+    } else if &input[..] == [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', '|', ' ', ' ', ' '] {
+        '1'
+    } else if &input[..] == [' ', '_', ' ', ' ', '_', '|', '|', '_', ' ', ' ', ' ', ' '] {
+        '2'
+    } else if &input[..] == [' ', '_', ' ', ' ', '_', '|', ' ', '_', '|', ' ', ' ', ' '] {
+        '3'
+    } else if &input[..] == [' ', ' ', ' ', '|', '_', '|', ' ', ' ', '|', ' ', ' ', ' '] {
+        '4'
+    } else if &input[..] == [' ', '_', ' ', '|', '_', ' ', ' ', '_', '|', ' ', ' ', ' '] {
+        '5'
+    } else if &input[..] == [' ', '_', ' ', '|', '_', ' ', '|', '_', '|', ' ', ' ', ' '] {
+        '6'
+    } else if &input[..] == [' ', '_', ' ', ' ', ' ', '|', ' ', ' ', '|', ' ', ' ', ' '] {
+        '7'
+    } else if &input[..] == [' ', '_', ' ', '|', '_', '|', '|', '_', '|', ' ', ' ', ' '] {
+        '8'
+    } else if &input[..] == [' ', '_', ' ', '|', '_', '|', ' ', '_', '|', ' ', ' ', ' '] {
+        '9'
+    } else {
+        '?'
+    }
 }
