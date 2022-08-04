450bd5172f4945315c97da6e395e3e7b8f22ae01
lizanbing <zanbingli@gmail.com>
2022-08-04 10:35:25 +0800

    管道解法

--- exercises/parallel-letter-frequency/src/lib.rs
+++ exercises/parallel-letter-frequency/src/lib.rs
@@ -1,8 +1,10 @@
-extern crate rayon;
+// extern crate rayon;
 
 use std::collections::HashMap;
+use std::sync::mpsc::channel;
+use std::thread;
 
-use rayon::prelude::*;
+// use rayon::prelude::*;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let mut rt = HashMap::new();
@@ -11,47 +13,79 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
             input.len() / worker_count + 1
         };
     // let mut threads = vec![];
-    //map
+    // //map
     // for sli in input.chunks(p_size) {
     //     let s = Arc::new(sli);
     //     threads.push(thread::spawn(move || -> HashMap<char, usize>{
     //         count_char(s.clone())
     //     }));
     // }
-
-    //reduce
+    //
+    // //reduce
     // for tr in threads {
     //     let mp = tr.join().unwrap();
     //     append(&mut rt, &mp);
     // }
 
-    let mut raws: Vec<(&[&str], HashMap<char, usize>)> = vec![];
+    //rayon solution
+    // let mut raws: Vec<(&[&str], HashMap<char, usize>)> = vec![];
+    //
+    // for v in input.chunks(p_size) {
+    //     let mmp = HashMap::new();
+    //     raws.push((v, mmp));
+    // }
+    //
+    // let raw_ref: Vec<(&[&str], &mut HashMap<char, usize>)> =
+    //     raws.iter_mut().map(|(k, v)| (k.clone(), v)).collect();
+    //
+    // raw_ref.into_par_iter()
+    //     .for_each(|(va, mut mp)| {
+    //         append(&mut mp, &count_char(va));
+    //     });
+    //
+    // //reduce
+    // for (_, mp) in raws {
+    //     append(&mut rt, &mp);
+    // }
 
+    // slot solution
+    let mut threads = vec![];
+    let (sender, receiver) = channel();
     for v in input.chunks(p_size) {
-        let mmp = HashMap::new();
-        raws.push((v, mmp));
+        let sd = sender.clone();
+        let s:String = v.iter().flat_map(|x|x.chars()).collect();
+        threads.push(
+            thread::spawn(move || {
+            sd.send(count_string(&s))
+        }));
     }
 
-    let raw_ref: Vec<(&[&str], &mut HashMap<char, usize>)> =
-        raws.iter_mut().map(|(k, v)| (k.clone(), v)).collect();
-
-    raw_ref.into_par_iter()
-        .for_each(|(va, mut mp)| {
-            append(&mut mp, &count_char(va));
-        });
-
-    //reduce
-    for (_, mp) in raws {
+    for _ in 0..input.chunks(p_size).len()  {
+        let mp = receiver.recv().unwrap();
         append(&mut rt, &mp);
     }
 
+
+    // //reduce
+    // for tr in threads {
+    //     tr.join().unwrap();
+    // }
     rt
 }
 
-fn count_char(sli: &[&str]) -> HashMap<char, usize> {
+// fn count_char(sli: &[&str]) -> HashMap<char, usize> {
+//     let mut rt = HashMap::new();
+//     for c in sli.iter()
+//         .flat_map(|x| x.chars()).collect::<String>().to_lowercase().chars()
+//         .filter(|x| x.is_alphabetic()) {
+//         *rt.entry(c).or_insert(0) += 1;
+//     }
+//     rt
+// }
+
+fn count_string(sli: &String) -> HashMap<char, usize> {
     let mut rt = HashMap::new();
-    for c in sli.iter()
-        .flat_map(|x|x.chars()).collect::<String>().to_lowercase().chars()
+    for c in sli.to_lowercase().chars()
         .filter(|x| x.is_alphabetic()) {
         *rt.entry(c).or_insert(0) += 1;
     }
